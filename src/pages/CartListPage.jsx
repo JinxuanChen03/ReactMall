6c9571ffc46f020b538e6b54a3ed24d318160d9c
莫猫猫的狗一只啦 <2334579301@qq.com>
2024-06-09 21:47:16 +0800

    hhhhhh

--- src/pages/CartListPage.jsx
+++ src/pages/CartListPage.jsx
@@ -9,153 +9,179 @@ import { useNavigate } from 'react-router-dom';
 const { Title, Text } = Typography;
 
 const CartListPage = () => {
-    useLoginCheck(); // 钩子调用
-    const [carts, setCarts] = useState([]);
-    const service = useContext(ServiceContext);
-    const navigate = useNavigate();
+  useLoginCheck(); // 钩子调用
+  const [carts, setCarts] = useState([]);
+  const service = useContext(ServiceContext);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchCartData = async () => {
-            const currentUser = service.user.getCurrentUser();
-            console.log('当前用户:', currentUser); // 添加日志检查当前用户
+  useEffect(() => {
+    const fetchCartData = async () => {
+      try
+      {
+        const currentUser = await service.user.getCurrentUser(); // 获取当前用户
+        console.log('当前用户:', currentUser); // 添加日志检查当前用户
 
-            if (currentUser) {
-                const userCarts = service.cart.getCartByUserId(currentUser.id);
-                console.log('用户购物车数据:', userCarts); // 添加日志检查用户购物车数据
+        if (currentUser)
+        {
+          const userCarts = await service.cart.getCartByUserId(currentUser.id); // 获取用户的购物车数据
+          console.log('用户购物车数据:', userCarts); // 添加日志检查用户购物车数据
 
-                const cartsWithDetails = userCarts.map(cart => {
-                    const good = service.good.getGoodById(cart.goodId);
-                    console.log('商品数据:', good); // 添加日志检查商品数据
+          // 使用 Promise.all 并行获取所有商品数据
+          const cartsWithDetails = await Promise.all(userCarts.map(async (cart) => {
+            try
+            {
+              const good = await service.good.getGoodById(cart.goodId); // 获取商品数据
+              console.log('商品数据:', good); // 添加日志检查商品数据
 
-                    if (good) {
-                        const type = good.types.find(type => type.typeid === cart.type);
-                        if (type) {
-                            return {
-                                ...cart,
-                                name: good.name,
-                                price: type.typePrice,
-                                image: require(`../static/temp/${good.img[0]}`), // 假设商品图片数组的第一个为主图
-                                checked: true,
-                            };
-                        } else {
-                            console.error(`商品ID为${cart.goodId}的类型${cart.type}未找到`);
-                            return null;
-                        }
-                    } else {
-                        console.error(`商品ID为${cart.goodId}的商品未找到`);
-                        return null;
-                    }
-                }).filter(cart => cart !== null); // 过滤掉未找到商品的购物车项
-                setCarts(cartsWithDetails);
-            }
-        };
-
-        fetchCartData();
-    }, [service]);
-
-    const handleQuantityChange = (id, quantity) => {
-        const currentUser = service.user.getCurrentUser();
-        if (currentUser) {
-            const updatedCarts = carts.map(cart => {
-                if (cart.id === id) {
-                    service.cart.updateQuantity(currentUser.id, cart.goodId, quantity);
-                    return { ...cart, quantity };
+              if (good)
+              {
+                const type = good.types.find(type => type.typeid === cart.type);
+                if (type)
+                {
+                  return {
+                    ...cart,
+                    name: good.name,
+                    price: type.typePrice,
+                    image: require(`../static/temp/${good.img[0]}`), // 假设商品图片数组的第一个为主图
+                    checked: true,
+                  };
+                } else
+                {
+                  console.error(`商品ID为${cart.goodId}的类型${cart.type}未找到`);
+                  return null;
                 }
-                return cart;
-            });
-            setCarts(updatedCarts);
-        }
-    };
-
-    const handleRemove = (id) => {
-        const currentUser = service.user.getCurrentUser();
-        if (currentUser) {
-            const cartItem = carts.find(cart => cart.id === id);
-            if (cartItem) {
-                service.cart.removeFromCart(currentUser.id, cartItem.goodId);
-                setCarts(carts.filter(cart => cart.id !== id));
+              } else
+              {
+                console.error(`商品ID为${cart.goodId}的商品未找到`);
+                return null;
+              }
+            } catch (error)
+            {
+              console.error('获取商品数据时出错:', error);
+              return null;
             }
-            message.success('商品已从购物车移除');
+          }));
+
+          setCarts(cartsWithDetails.filter(cart => cart !== null)); // 过滤掉未找到商品的购物车项
         }
+      } catch (error)
+      {
+        console.error('获取购物车数据时出错:', error);
+        message.error('加载购物车数据时出错');
+      }
     };
 
-    const handleCheck = (id) => {
-        const updatedCarts = carts.map(cart => cart.id === id ? { ...cart, checked: !cart.checked } : cart);
-        setCarts(updatedCarts);
-    };
+    fetchCartData();
+  }, [service]);
 
-    const handleCheckAll = () => {
-        const newChecked = !carts.every(cart => cart.checked);
-        const updatedCarts = carts.map(cart => ({ ...cart, checked: newChecked }));
-        setCarts(updatedCarts);
-    };
-
-    const getTotalPrice = () => {
-        return carts.reduce((total, cart) => cart.checked ? total + cart.price * cart.quantity : total, 0);
-    };
-
-    const goBack = () => {
-        // TBD 跳转到之前的页面
-    };
-
-    const proceedToCheckout = () => {
-        const selectedCarts = carts.filter(cart => cart.checked);
-        if (selectedCarts.length === 0) {
-            message.warning('请选择至少一个商品进行结算');
-            return;
+  const handleQuantityChange = async (id, quantity) => {
+    const currentUser = await service.user.getCurrentUser();
+    if (currentUser)
+    {
+      const updatedCarts = carts.map(cart => {
+        if (cart.id === id)
+        {
+          service.cart.updateQuantity(currentUser.id, cart.goodId, quantity);
+          return { ...cart, quantity };
         }
-        // 假设我们将所选商品的ID、类型和数量传递给订单页面
-        selectedCarts.forEach(cart => {
-            navigate(`/createOrder/${cart.goodId}/${cart.type}/${cart.quantity}`);
-        });
-    };
+        return cart;
+      });
+      setCarts(updatedCarts);
+    }
+  };
 
-    return (
-        <>
-            <TopNavBar onBack={goBack} title="购物车" />
-            <div style={{ backgroundColor: '#f5f5f5', height: '100vh', overflowY: 'auto', display: 'flex', flexDirection: 'column' }}>
-                <div style={{ flex: '1 1 auto', padding: '20px' }}>
-                    <Card style={{ backgroundColor: '#ffffff', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', borderRadius: '8px', padding: '20px' }}>
-                        <Title level={2}>购物车</Title>
-                        {carts.length > 0 ? (
-                            <List
-                                itemLayout="horizontal"
-                                dataSource={carts}
-                                renderItem={cart => (
-                                    <List.Item
-                                        actions={[
-                                            <InputNumber
-                                                min={1}
-                                                value={cart.quantity}
-                                                onChange={(value) => handleQuantityChange(cart.id, value)}
-                                                style={{ width: 60, marginRight: 16 }}
-                                            />,
-                                            <Button type="link" onClick={() => handleRemove(cart.id)}>删除</Button>
-                                        ]}
-                                    >
-                                        <List.Item.Meta
-                                            avatar={<img src={cart.image} alt={cart.name} style={{ width: '50px' }} />}
-                                            title={<span style={{ display: 'inline-block', maxWidth: '100px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>{cart.name}</span>}
-                                            description={`¥${cart.price}`}
-                                        />
-                                        <Checkbox checked={cart.checked} onChange={() => handleCheck(cart.id)} />
-                                    </List.Item>
-                                )}
-                            />
-                        ) : (
-                            <Text>购物车为空</Text>
-                        )}
-                        <Divider />
-                        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#ffffff', padding: '10px 20px', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
-                            <Checkbox onChange={handleCheckAll} checked={carts.every(cart => cart.checked)}>全选</Checkbox>
-                            <Text>总价: ¥{getTotalPrice()}</Text>
-                            <Button type="primary" onClick={proceedToCheckout}>去结算</Button>
-                        </div>
-                    </Card>
-                </div>
+  const handleRemove = async (id) => {
+    const currentUser = await service.user.getCurrentUser();
+    if (currentUser)
+    {
+      const cartItem = carts.find(cart => cart.id === id);
+      if (cartItem)
+      {
+        await service.cart.removeFromCart(currentUser.id, cartItem.goodId);
+        setCarts(carts.filter(cart => cart.id !== id));
+      }
+      message.success('商品已从购物车移除');
+    }
+  };
+
+  const handleCheck = (id) => {
+    const updatedCarts = carts.map(cart => cart.id === id ? { ...cart, checked: !cart.checked } : cart);
+    setCarts(updatedCarts);
+  };
+
+  const handleCheckAll = () => {
+    const newChecked = !carts.every(cart => cart.checked);
+    const updatedCarts = carts.map(cart => ({ ...cart, checked: newChecked }));
+    setCarts(updatedCarts);
+  };
+
+  const getTotalPrice = () => {
+    return carts.reduce((total, cart) => cart.checked ? total + cart.price * cart.quantity : total, 0);
+  };
+
+  const goBack = () => {
+    navigate(-1);// TBD 跳转到之前的页面
+  };
+
+  const proceedToCheckout = () => {
+    const selectedCarts = carts.filter(cart => cart.checked);
+    if (selectedCarts.length === 0)
+    {
+      message.warning('请选择至少一个商品进行结算');
+      return;
+    }
+    // 假设我们将所选商品的ID、类型和数量传递给订单页面
+    selectedCarts.forEach(cart => {
+      navigate(`/createOrder/${cart.goodId}/${cart.type}/${cart.quantity}`);
+    });
+  };
+
+  return (
+    <>
+      <TopNavBar onBack={goBack} title="购物车" />
+      <div style={{ backgroundColor: '#f5f5f5', height: '100vh', overflowY: 'auto', display: 'flex', flexDirection: 'column' }}>
+        <div style={{ flex: '1 1 auto', padding: '20px' }}>
+          <Card style={{ backgroundColor: '#ffffff', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', borderRadius: '8px', padding: '20px' }}>
+            <Title level={2}>购物车</Title>
+            {carts.length > 0 ? (
+              <List
+                itemLayout="horizontal"
+                dataSource={carts}
+                renderItem={cart => (
+                  <List.Item
+                    actions={[
+                      <InputNumber
+                        min={1}
+                        value={cart.quantity}
+                        onChange={(value) => handleQuantityChange(cart.id, value)}
+                        style={{ width: 60, marginRight: 16 }}
+                      />,
+                      <Button type="link" onClick={() => handleRemove(cart.id)}>删除</Button>
+                    ]}
+                  >
+                    <List.Item.Meta
+                      avatar={<img src={cart.image} alt={cart.name} style={{ width: '50px' }} />}
+                      title={<span style={{ display: 'inline-block', maxWidth: '100px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>{cart.name}</span>}
+                      description={`¥${cart.price}`}
+                    />
+                    <Checkbox checked={cart.checked} onChange={() => handleCheck(cart.id)} />
+                  </List.Item>
+                )}
+              />
+            ) : (
+              <Text>购物车为空</Text>
+            )}
+            <Divider />
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#ffffff', padding: '10px 20px', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+              <Checkbox onChange={handleCheckAll} checked={carts.every(cart => cart.checked)}>全选</Checkbox>
+              <Text>总价: ¥{getTotalPrice()}</Text>
+              <Button type="primary" onClick={proceedToCheckout}>去结算</Button>
             </div>
-        </>
-    );
+          </Card>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CartListPage;
--- src/pages/OrderListPage.jsx
+++ src/pages/OrderListPage.jsx
@@ -39,110 +39,118 @@ const { Title } = Typography;
 const { TabPane } = Tabs;
 
 const OrderListPage = () => {
-    useLoginCheck(); // 钩子调用
-    const [orders, setOrders] = useState([]);
-    const service = useContext(ServiceContext);
-    const navigate = useNavigate();
+  useLoginCheck(); // 钩子调用
+  const [orders, setOrders] = useState([]);
+  const service = useContext(ServiceContext);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchOrderData = async () => {
-            const currentUser = service.user.getCurrentUser();
-            if (currentUser) {
-                const userOrders = service.order.getOrdersByUserId(currentUser.id);
-                setOrders(userOrders);
-            }
-        };
-
-        fetchOrderData();
-    }, [service]);
-
-    const handleViewDetails = (orderId) => {
-        navigate(`/orderDetail/${orderId}`);
-    };
-
-    const filterOrders = (status) => {
-        if (status === 'all') {
-            return orders;
+  useEffect(() => {
+    const fetchOrderData = async () => {
+      try
+      {
+        const currentUser = await service.user.getCurrentUser(); // 获取当前用户信息
+        if (currentUser)
+        {
+          const userOrders = await service.order.getOrdersByUserId(currentUser.id); // 获取用户订单数据
+          setOrders(userOrders);
         }
-        return orders.filter(order => order.status === status);
+      } catch (error)
+      {
+        console.error('获取订单数据时出错:', error);
+      }
     };
 
-    const goBack = () => {
-        navigate(-1); // Navigate back to the previous page
-    };
+    fetchOrderData();
+  }, [service]);
 
-    const goToHome = () => {
-        navigate('/home'); // Navigate to the home page
-    };
+  const handleViewDetails = (orderId) => {
+    navigate(`/orderDetail/${orderId}`);
+  };
 
-    return (
-        <>
-            <TopNavBar onBack={goBack} onHome={goToHome} title="我的订单" />
-            <div style={styles.container}>
-                <div style={{ flex: '1 1 auto', padding: '20px' }}>
-                    <Card style={styles.card}>
-                        <Title level={2} style={styles.title}>我的订单</Title>
-                        <Tabs defaultActiveKey="all">
-                            <TabPane tab="全部订单" key="all">
-                                <OrderList orders={filterOrders('all')} onViewDetails={handleViewDetails} />
-                            </TabPane>
-                            <TabPane tab="待付款" key="pending">
-                                <OrderList orders={filterOrders(0)} onViewDetails={handleViewDetails} />
-                            </TabPane>
-                            <TabPane tab="待收货" key="shipped">
-                                <OrderList orders={filterOrders(1)} onViewDetails={handleViewDetails} />
-                            </TabPane>
-                            <TabPane tab="退款/售后" key="refund">
-                                <OrderList orders={filterOrders(2)} onViewDetails={handleViewDetails} />
-                            </TabPane>
-                        </Tabs>
-                    </Card>
-                </div>
-            </div>
-        </>
-    );
+  const filterOrders = (status) => {
+    if (status === 'all')
+    {
+      return orders;
+    }
+    return orders.filter(order => order.status === status);
+  };
+
+  const goBack = () => {
+    navigate(-1); // Navigate back to the previous page
+  };
+
+  const goToHome = () => {
+    navigate('/home'); // Navigate to the home page
+  };
+
+  return (
+    <>
+      <TopNavBar onBack={goBack} onHome={goToHome} title="我的订单" />
+      <div style={styles.container}>
+        <div style={{ flex: '1 1 auto', padding: '20px' }}>
+          <Card style={styles.card}>
+            <Title level={2} style={styles.title}>我的订单</Title>
+            <Tabs defaultActiveKey="all">
+              <TabPane tab="全部订单" key="all">
+                <OrderList orders={filterOrders('all')} onViewDetails={handleViewDetails} />
+              </TabPane>
+              <TabPane tab="待付款" key="pending">
+                <OrderList orders={filterOrders(0)} onViewDetails={handleViewDetails} />
+              </TabPane>
+              <TabPane tab="待收货" key="shipped">
+                <OrderList orders={filterOrders(1)} onViewDetails={handleViewDetails} />
+              </TabPane>
+              <TabPane tab="退款/售后" key="refund">
+                <OrderList orders={filterOrders(2)} onViewDetails={handleViewDetails} />
+              </TabPane>
+            </Tabs>
+          </Card>
+        </div>
+      </div>
+    </>
+  );
 };
 
 const OrderList = ({ orders, onViewDetails }) => (
-    <List
-        itemLayout="horizontal"
-        dataSource={orders}
-        renderItem={order => (
-            <List.Item
-                actions={[
-                    <Button type="link" onClick={() => onViewDetails(order.id)}>查看详情</Button>
-                ]}
-            >
-                <List.Item.Meta
-                    title={`订单号: ${order.orderNo}`}
-                    description={`创建时间: ${order.createTime}`}
-                />
-                <div>{order.status === 1 ? '已支付' : '未支付'}</div>
-            </List.Item>
-        )}
-    />
+  <List
+    itemLayout="horizontal"
+    dataSource={orders}
+    renderItem={order => (
+      <List.Item
+        actions={[
+          <Button type="link" onClick={() => onViewDetails(order.id)}>查看详情</Button>
+        ]}
+      >
+        <List.Item.Meta
+          title={`订单号: ${order.orderNo}`}
+          description={`创建时间: ${order.createTime}`}
+        />
+        <div>{order.status === 1 ? '已支付' : '未支付'}</div>
+      </List.Item>
+    )}
+  />
 );
 
 const styles = {
-    container: {
-        backgroundColor: '#f5f5f5',
-        height: '100vh',
-        overflowY: 'auto',
-        display: 'flex',
-        flexDirection: 'column'
-    },
-    title: {
-        textAlign: 'center',
-        marginBottom: '20px'
-    },
-    card: {
-        backgroundColor: '#ffffff',
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-        borderRadius: '8px',
-        padding: '20px',
-        maxWidth: '800px',
-        margin: '0 auto'
-    }
+  container: {
+    backgroundColor: '#f5f5f5',
+    height: '100vh',
+    overflowY: 'auto',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  title: {
+    textAlign: 'center',
+    marginBottom: '20px'
+  },
+  card: {
+    backgroundColor: '#ffffff',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    borderRadius: '8px',
+    padding: '20px',
+    maxWidth: '800px',
+    margin: '0 auto'
+  }
 };
 
 export default OrderListPage;
